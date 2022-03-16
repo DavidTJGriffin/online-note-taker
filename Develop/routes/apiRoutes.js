083d93d9d40a57bf034f91f577b341848680c013
David Griffin <david.dtjg2001@gmail.com>
2022-03-15 18:39:40 -0700

    update

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
 //GET  - /api/notes'
 router.get('/notes', (req, res) => {
@@ -15,10 +16,27 @@ router.get('/notes', (req, res) => {
     })
 });
 
+function createNewNote (body, notesArray) {
+    const createNote = body;
+    notesArray.push(createNote);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ createNote: notesArray }, null, 2)
+    );
+    return createNote;
+  
+}
+
 //POST -/api/notes'
 router.post('/notes', (req, res) => {
-    const createNote = req.body;
+    req.body.id = notes.length.toString();
+
+    const createNote = createNewNote(req.body, createNote);
+    res.json(createNote);
+    
+
     console.log(createNote);
+   
 });
 
 //DELETE  -/api/notes/:id'
